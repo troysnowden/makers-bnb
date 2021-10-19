56d3796dcf12bdebf35da4c0ad7a0dea2045fe1c
Troy Snowden <troybradsnowden@gmail.com>
2021-10-19 16:26:27 +0100

    Remove getting ahead of myself code

--- lib/database_connection.rb
+++
@@ -1,8 +0,0 @@
-class DatabaseConnection
-  class << self
-    def connect_to_db
-      database_name = ENV['RACK_ENV'] == 'development' ? 'mbnb' : 'mbnb_test'
-      PG.connect dbname: database_name, user: ENV['USER']
-    end
-  end
-end
