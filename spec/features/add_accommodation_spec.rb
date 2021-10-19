285ecf063f9afbef230eadc5c384955648f3c4ff
Troy Snowden <troybradsnowden@gmail.com>
2021-10-19 12:16:09 +0100

    Fix feature test for add accommodation

--- spec/features/add_accommodation_spec.rb
+++ spec/features/add_accommodation_spec.rb
@@ -5,6 +5,8 @@ feature 'add accommodation' do
         fill_in('description', with: '2 bedroom cottage')
         fill_in('price_per_night', with: '50')
         click_button('Submit')
+
+        ## TODO: assert that accommodation is added to list of user's accommodation
     end
 
 end
