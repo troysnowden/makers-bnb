8d8058e67e5bc2bf2de56424ba455003520e2f0e
khalid <khalidmiah@hotmail.co.uk>
2021-10-19 16:32:47 +0100

    refactored user class tests

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -6,32 +6,33 @@ describe User do
       it "should be an instance of user" do
         expect(described_class.new(01,'tom','s').class).to eq(User)
       end
-
+      
       it "should initalize with correct attributes" do
-        test_booking = described_class.new(01,'tom','s')
-        expect(test_booking.user_ID).to eq 01
-        expect(test_booking.first_name).to eq "tom"
-        expect(test_booking.last_name).to eq "s"
-
-      end
+        test_user = described_class.new(01,'tom','s')
+        expect(test_user).to have_attributes(
+            user_ID: 01,
+            first_name: "tom",
+            last_name: "s"
+        )
+      end   
 
       it "should check for method bookings" do
-        test_booking = described_class.new(01,'tom','s')
+        test_user = described_class.new(01,'tom','s')
 
-        expect(test_booking).to respond_to :bookings
+        expect(test_user).to respond_to :bookings
       end 
 
       it "should check for method all_accommodation" do
-        test_booking = described_class.new(01,'tom','s')
+        test_user = described_class.new(01,'tom','s')
 
-        expect(test_booking).to respond_to :all_accommodation
+        expect(test_user).to respond_to :all_accommodation
       end 
 
 
       it "should check for method approve_booking" do
-        test_booking = described_class.new(01,'tom','s')
+        test_user = described_class.new(01,'tom','s')
 
-        expect(test_booking).to respond_to :approve_booking
+        expect(test_user).to respond_to :approve_booking
       end 
     
     end 
