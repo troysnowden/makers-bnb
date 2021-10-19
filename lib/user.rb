d1aba1f8d339ce4ebc320cc8d5c2405b676e3dfd
khalid <khalidmiah@hotmail.co.uk>
2021-10-19 16:16:19 +0100

    add user class method tests

--- lib/user.rb
+++ lib/user.rb
@@ -1,12 +1,23 @@
 class User
-    attr_reader :first_name , :last_name , :user_ID
+    attr_reader :first_name , :last_name , :user_ID 
     def initialize(user_ID, first_name, last_name)
         @user_ID = user_ID
         @first_name = first_name
         @last_name = last_name
-        
 
     end  
+
+    def bookings
+
+    end 
+    
+    def all_accommodation
+
+    end     
+
+    def approve_booking(booking_id)
+
+    end     
     
 
 
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,6 +2,7 @@ require 'user'
 
 describe User do
     describe "#initialize" do
+    
       it "should be an instance of user" do
         expect(described_class.new(01,'tom','s').class).to eq(User)
       end
@@ -12,15 +13,26 @@ describe User do
         expect(test_booking.first_name).to eq "tom"
         expect(test_booking.last_name).to eq "s"
 
-  
-        # expect(test_booking).to have_attributes(
-        #   :accom_id => 1,
-        #   :visitor_id => 2,
-        #   :total_cost => 10, 
-        #   :arrival_date => '01/02/2022',
-        #   :departure_date => '11/02/2022',
-        #   :confirmed => false
-        # )
       end
+
+      it "should check for method bookings" do
+        test_booking = described_class.new(01,'tom','s')
+
+        expect(test_booking).to respond_to :bookings
+      end 
+
+      it "should check for method all_accommodation" do
+        test_booking = described_class.new(01,'tom','s')
+
+        expect(test_booking).to respond_to :all_accommodation
+      end 
+
+
+      it "should check for method approve_booking" do
+        test_booking = described_class.new(01,'tom','s')
+
+        expect(test_booking).to respond_to :approve_booking
+      end 
+    
     end 
 end       
