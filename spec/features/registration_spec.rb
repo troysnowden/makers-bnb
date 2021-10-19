8d3df509aad3301939513bbbc49ef1d59d245097
Tom Baxter <thomas99baxter@gmail.com>
2021-10-19 12:05:23 +0100

    add tests

---
+++ spec/features/registration_spec.rb
@@ -0,0 +1,10 @@
+feature 'registration' do
+  it 'should have a page with a form' do
+    visit '/register'
+    expect(page).to have_content("MakersBNB")
+    expect(page).to have_field("first-name")
+    expect(page).to have_field("last-name")
+
+    # click_button "Submit"
+  end
+end
