2fc207941e217f4dc57564cdee46f67db7a2c13f
Tom Baxter <thomas99baxter@gmail.com>
2021-10-19 15:13:04 +0100

    please troy

--- lib/booking.rb
+++ lib/booking.rb
@@ -1,7 +1,8 @@
 class Booking
-  attr_reader :accom_id, :total_cost, :arrival_date, :departure_date, :confirmed
-  def initialize(accom_id, total_cost, arrival_date, departure_date)
+  attr_reader :accom_id, :visitor_id, :total_cost, :arrival_date, :departure_date, :confirmed
+  def initialize(accom_id, visitor_id, total_cost, arrival_date, departure_date)
     @accom_id = accom_id
+    @visitor_id = visitor_id
     @total_cost = total_cost
     @arrival_date = arrival_date
     @departure_date = departure_date
--- spec/booking_spec.rb
+++ spec/booking_spec.rb
@@ -3,14 +3,15 @@ require 'booking'
 describe Booking do
   describe "#initialize" do
     it "should be an instance of booking" do
-      expect(described_class.new(1,10,'01/02/2022','11/02/2022').class).to eq(Booking)
+      expect(described_class.new(1,2,10,'01/02/2022','11/02/2022').class).to eq(Booking)
     end
 
     it "should initalize with correct attributes" do
-      test_booking = described_class.new(1,10,'01/02/2022','11/02/2022')
+      test_booking = described_class.new(1,2,10,'01/02/2022','11/02/2022')
 
       expect(test_booking).to have_attributes(
         :accom_id => 1,
+        :visitor_id => 2,
         :total_cost => 10, 
         :arrival_date => '01/02/2022',
         :departure_date => '11/02/2022',
@@ -21,7 +22,7 @@ describe Booking do
 
   describe "#confirm" do
     it "should change confirmed from true to false" do
-      test_booking = described_class.new(1,10,'01/02/2022','11/02/2022')
+      test_booking = described_class.new(1,2,10,'01/02/2022','11/02/2022')
 
       expect{
         test_booking.confirm
