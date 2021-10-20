5ef098467b9020abd354ea71dc9c5956a1074663
Tom Baxter <thomas99baxter@gmail.com>
2021-10-20 11:17:42 +0100

    fix: broken test on user.rb

--- spec/features/view_home_page_spec.rb
+++ spec/features/view_home_page_spec.rb
@@ -3,6 +3,7 @@
 feature 'Viewing homepage' do
   scenario 'visits homepage' do
     visit('/')
+    expect(page).to have_content('MakersBNB')
     expect(page).to have_content('Manage my accommodation')
   end
 end
