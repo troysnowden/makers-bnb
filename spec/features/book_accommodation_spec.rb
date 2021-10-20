f3aac555dcfb4f1a5c9a5a779f0eb3e22a0db34d
Tom Suckling <tomsuckling@Toms-MacBook-Air.local>
2021-10-20 11:57:55 +0100

    added test for max price slider

--- spec/features/book_accommodation_spec.rb
+++ spec/features/book_accommodation_spec.rb
@@ -4,4 +4,11 @@ feature 'book accommodation' do
       fill_in 'calendar', with: 'date'
       click_button('Search')
     end
+
+    scenario 'Should be able to select a max price' do
+      visit '/book-accommodation'
+      fill_in 'calendar', with: 'date'
+      fill_in 'max_price', with: '80'
+      click_button('Search')
+    end
 end
