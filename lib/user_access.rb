b320de278814138cc6f27be4b6ecf39598bb31d6
Troy Snowden <troybradsnowden@gmail.com>
2021-10-20 15:05:32 +0100

    Add login method + plan and rename register method

--- lib/user_access.rb
+++ lib/user_access.rb
@@ -4,13 +4,17 @@ class UserAccess
 
     class << self
 
-      def user_registartion(first_name, last_name)
+      def register(first_name, last_name)
         result = DatabaseConnection.connect_to_db.exec_params(
         "INSERT INTO users (first_name, last_name) VALUES($1, $2) RETURNING id, first_name, last_name;", [first_name, last_name])
         
         User.new(result[0]['id'], result[0]['first_name'], result[0]['last_name'])
-      end  
+      end 
       
+      def login(first_name, last_name)
+        ## check given names are in db, if so return user
+      end
+
     end
 
 end 
--- spec/user_access_spec.rb
+++ spec/user_access_spec.rb
@@ -3,7 +3,7 @@ describe UserAccess do
     describe '#user_registration' do
 
         it 'should regiser user with first name and last name' do
-            new_user = UserAccess.user_registartion('troy','snowdon')
+            new_user = UserAccess.register('troy','snowdon')
             expect(new_user.class).to eq (User)
             expect(new_user.first_name).to eq "troy"
             expect(new_user.last_name).to eq "snowdon"
