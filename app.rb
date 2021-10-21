6a9b74f15bb7f02a9f72b620cef7253b108d0deb
Tom Suckling <tomsuckling@Toms-MacBook-Air.local>
2021-10-21 11:39:35 +0100

    added functionality to submit button

--- app.rb
+++ app.rb
@@ -44,6 +44,11 @@ class MakersBnb < Sinatra::Base
     erb (:manage_accommodation)
   end
 
+  post "/manage-accommodation" do
+    # add accommodation to db
+    redirect "/manage-accommodation"
+  end
+
   get "/add-accommodation" do
     erb :add_accommodation
   end
