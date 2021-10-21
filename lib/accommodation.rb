06bd95bc386919db7191bc6f6ad6c79588827ef2
amin-newcastle <muhammad-karim@hotmail.com>
2021-10-21 17:10:36 +0100

    connected add accom to db

--- lib/accommodation.rb
+++ lib/accommodation.rb
@@ -1,3 +1,5 @@
+require_relative 'database_connection'
+
 class Accommodation
     attr_reader :accommodation_id, :owner_id, :name, :description, :price_per_night
 
@@ -7,6 +9,15 @@ class Accommodation
         @name = name
         @description = description
         @price_per_night = price
+    end
+
+    class << self
+        def create(owner_id, name, description, price_per_night)
+            result = DatabaseConnection.connect_to_db.exec_params(
+            "INSERT INTO accommodations (owner_id, name, description, price_per_night) VALUES($1, $2, $3, $4) RETURNING id, owner_id, name, description, price_per_night;", [owner_id, name, description, price_per_night])
+            
+            Accommodation.new(result[0]['id'], result[0]['owner_id'], result[0]['name'], result[0]['description'], result[0]['price_per_night'])
+        end 
     end
     
 end
