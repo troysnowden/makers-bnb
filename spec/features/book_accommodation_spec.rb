b5c5bc6654270ed68ac75d0d97be5d1f41f9dd69
Tom Baxter <thomas99baxter@gmail.com>
2021-10-21 14:30:24 +0100

    fix tests

--- spec/features/book_accommodation_spec.rb
+++ spec/features/book_accommodation_spec.rb
@@ -9,5 +9,24 @@ feature 'book accommodation' do
       expect(page).to have_content('Please enter your max price:')
     end
 
+    scenario 'Should be able to see an accom' do
+      visit '/book-accommodation'
+      expect(page).to have_content('Sunny Sands')
+      expect(page).to have_content('coarse and rough')
+      expect(page).to have_content('£80 per night')
+      expect(page).to have_selector("input[type=submit][value='Book Sunny Sands']")
+      expect(page).to have_selector("input[type=submit][id='submit-booking-1']")
+    end
     
+    scenario 'Should be able to see an accom, and book it' do
+      visit '/book-accommodation'
+      expect(page).to have_content('Sunny Sands')
+      expect(page).to have_content('coarse and rough')
+      expect(page).to have_content('£80 per night')
+      expect(page).to have_selector("input[type=submit][value='Book Sunny Sands']")
+      expect(page).to have_selector("input[type=submit][id='submit-booking-1']")
+      click_button('Book Sunny Sands')
+
+      expect(page).to have_current_path("/")
+    end
 end
