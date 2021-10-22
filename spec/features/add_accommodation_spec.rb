7d7a786453f5cd2a00e4cfdaa0aacc88ce0fde83
amin-newcastle <muhammad-karim@hotmail.com>
2021-10-22 10:19:25 +0100

    Added DB connection to add accom page

--- spec/features/add_accommodation_spec.rb
+++ spec/features/add_accommodation_spec.rb
@@ -1,13 +1,17 @@
 # frozen_string_literal: true
 
 feature 'add accommodation' do
-  scenario 'Should be able to submit vaild accommodation' do
+  scenario 'Should add new accommodation to the list of users accommodation' do
     visit '/add-accommodation'
-    fill_in('name', with: 'My accommodation')
-    fill_in('description', with: '2 bedroom cottage')
-    fill_in('price_per_night', with: '50')
-    click_button('Submit')
-
-    ## TODO: assert that accommodation is added to list of user's accommodation
+    expect(page).to have_field("name")
+    expect(page).to have_field("description")
+    expect(page).to have_field("price_per_night")
+    fill_in('name', with: 'My Villa')
+    fill_in('description', with: '4 bedroom villa')
+    fill_in('price_per_night', with: '100')
+    click_button "Submit"
+    expect(page).to have_content("Booking requests")
   end
+
+  
 end
