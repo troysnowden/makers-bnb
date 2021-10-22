e834e74502a1746623bc2914b1fd2194e6776264
amin-newcastle <muhammad-karim@hotmail.com>
2021-10-22 11:30:08 +0100

    added booking request test

--- spec/features/view_bookings_spec.rb
+++ spec/features/view_bookings_spec.rb
@@ -1,11 +1,23 @@
 feature 'Viewing my booking' do
     scenario 'Should be able to see a list of bookings which need approval' do
+      new_user = UserAccess.register('troy','snowdon')
+      new_accommodation = Accommodation.create(new_user.user_id, "My Villa", "4 Bed", 100)
+      new_booking = BookingAccess.create(new_user.user_id, new_accommodation.accommodation_id, 100, 2021-12-10)
       visit('/view-bookings')
-      expect(page).to have_content('Bookings requested')
+      expect(page).to have_content('My Villa')
+      expect(page).to have_content('4 Bed')
+      expect(page).to have_content('100')
+      expect(page).to have_content('2021-12-10')
     end
 
-    scenario 'Should be able to see a list of bookings which have been approvde' do
-        visit('/view-bookings')
-        expect(page).to have_content('Confirmed bookings')
-    end
+    # scenario 'Should be able to see a list of bookings which have been approved' do
+    #   new_user = UserAccess.register('troy','snowdon')
+    #   new_accommodation = Accommodation.create(new_user.user_id, "My Villa", "4 Bed", 100)
+    #   new_booking = BookingAccess.create(new_user.user_id, new_accommodation.accommodation_id, 100, 2021-12-10)
+    #   visit('/view-bookings')
+    #   expect(page).to have_content('My Villa')
+    #   expect(page).to have_content('4 Bed')
+    #   expect(page).to have_content('100')
+    #   expect(page).to have_content('2021-12-10')
+    # end
   end
